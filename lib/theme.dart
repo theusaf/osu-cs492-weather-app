9358bcf918e03faa152bc9623d3edd5547372154
theusaf <theusafyt@gmail.com>
2024-03-15 19:43:02 -0700

    feat: add sounds

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,29 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const _textThemeBase = TextTheme(
+  displayLarge: TextStyle(
+    fontSize: 60.0,
+    fontWeight: FontWeight.bold,
+  ),
+  displayMedium: TextStyle(
+    fontSize: 40.0,
+    fontWeight: FontWeight.bold,
+  ),
+  displaySmall: TextStyle(
+    fontSize: 30.0,
+    fontWeight: FontWeight.bold,
+  ),
+  bodyLarge: TextStyle(
+    fontSize: 16.0,
+  ),
+  bodyMedium: TextStyle(
+    fontSize: 14.0,
+  ),
+  bodySmall: TextStyle(
+    fontSize: 12.0,
+  ),
+  titleLarge: TextStyle(
+    fontSize: 20.0,
+    fontWeight: FontWeight.bold,
+  ),
+  titleMedium: TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  ),
+  titleSmall: TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  ),
+  labelLarge: TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+  ),
+  labelMedium: TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  ),
+  labelSmall: TextStyle(
+    fontSize: 10.0,
+    fontWeight: FontWeight.bold,
+  ),
+  headlineLarge: TextStyle(
+    fontSize: 32.0,
+    fontWeight: FontWeight.bold,
+  ),
+  headlineMedium: TextStyle(
+    fontSize: 26.0,
+    fontWeight: FontWeight.bold,
+  ),
+  headlineSmall: TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  ),
+);
 
 final lightTheme = ThemeData.from(
-    colorScheme: ColorScheme.light(
-  primary: Colors.green,
-  secondary: Colors.greenAccent,
-  onPrimary: Colors.white,
-  onSecondary: Colors.white,
-  surface: Colors.lightGreen.shade100,
-  onSurface: Colors.black,
-  background: Colors.white,
-  onBackground: Colors.black,
-  error: Colors.red,
-  onError: Colors.white,
-));
+  textTheme: GoogleFonts.convergenceTextTheme(_textThemeBase),
+  colorScheme: ColorScheme.light(
+    primary: Colors.green,
+    secondary: Colors.greenAccent,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    surface: Colors.lightGreen.shade100,
+    onSurface: Colors.black,
+    background: Colors.white,
+    onBackground: Colors.black,
+    error: Colors.red,
+    onError: Colors.white,
+  ),
+);
 
 final darkTheme = ThemeData.from(
-    colorScheme: ColorScheme.dark(
-  primary: Colors.green,
-  secondary: Colors.greenAccent,
-  onPrimary: Colors.white,
-  onSecondary: Colors.white,
-  surface: Colors.green.shade700,
-  onSurface: Colors.white,
-  background: Colors.green.shade900,
-  onBackground: Colors.white,
-  error: Colors.red,
-  onError: Colors.white,
-));
+  textTheme: GoogleFonts.convergenceTextTheme(_textThemeBase),
+  colorScheme: ColorScheme.dark(
+    primary: Colors.green,
+    secondary: Colors.greenAccent,
+    onPrimary: Colors.white,
+    onSecondary: Colors.white,
+    surface: Colors.green.shade700,
+    onSurface: Colors.white,
+    background: Colors.green.shade900,
+    onBackground: Colors.white,
+    error: Colors.red,
+    onError: Colors.white,
+  ),
+);
